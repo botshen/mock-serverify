58bc67f66691f3926c6bd5f0234783412797b894
Xin shen <363088847@qq.com>
2023-12-20 20:02:03 +0800

    feat:  add card

--- tabs/components/nav/index.tsx
+++ tabs/components/nav/index.tsx
@@ -23,13 +23,13 @@ export default function NavBar() {
           </li>
         </ul>
       </div>
-      <div className="form-control">
+      {/* <div className="form-control">
         <input
           type="text"
           placeholder="Search"
           className="input input-bordered w-24 md:w-auto"
         />
-      </div>
+      </div> */}
     </div>
   )
 }
--- tabs/projectList.tsx
+++ tabs/projectList.tsx
@@ -8,6 +8,62 @@ export default function ProjectList() {
   return (
     <>
       <h1>项目列表</h1>
+      <div className="grid grid-cols-3 gap-3 p-10">
+        <div className="card w-82 bg-base-100 shadow-xl">
+          <div className="card-body">
+            <h2 className="card-title">Card title!</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Buy Now</button>
+            </div>
+          </div>
+        </div>
+        <div className="card w-82 bg-base-100 shadow-xl">
+          <div className="card-body">
+            <h2 className="card-title">Card title!</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Buy Now</button>
+            </div>
+          </div>
+        </div>
+        <div className="card w-82 bg-base-100 shadow-xl">
+          <div className="card-body">
+            <h2 className="card-title">Card title!</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Buy Now</button>
+            </div>
+          </div>
+        </div>
+        <div className="card w-82 bg-base-100 shadow-xl">
+          <div className="card-body">
+            <h2 className="card-title">Card title!</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Buy Now</button>
+            </div>
+          </div>
+        </div>
+        <div className="card w-82 bg-base-100 shadow-xl">
+          <div className="card-body">
+            <h2 className="card-title">Card title!</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Buy Now</button>
+            </div>
+          </div>
+        </div>
+        <div className="card w-82 bg-base-100 shadow-xl">
+          <div className="card-body">
+            <h2 className="card-title">Card title!</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Buy Now</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
