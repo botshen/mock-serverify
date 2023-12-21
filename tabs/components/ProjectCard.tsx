8e3f294a9bb2d7dd85a6bfc397d99af9b35a2c38
Xin shen <363088847@qq.com>
2023-12-21 23:47:02 +0800

    wip: modal

--- tabs/components/ProjectCard.tsx
+++ tabs/components/ProjectCard.tsx
@@ -1,27 +1,118 @@
-import { Card, CardBody, Heading, Text } from "@chakra-ui/react"
+import {
+  Button,
+  Card,
+  CardBody,
+  FormControl,
+  FormErrorMessage,
+  FormHelperText,
+  FormLabel,
+  Heading,
+  HStack,
+  Input,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Spacer,
+  Text,
+  useDisclosure
+} from "@chakra-ui/react"
+import { Field, Form, Formik } from "formik"
 import { useNavigate } from "react-router-dom"
 
 interface Props {
   name: string
   description: string
-  image?: string
+  baseUrl?: string
 }
-const ProjectCard = ({ name, description }: Props) => {
+const ProjectCard = ({ name, description, baseUrl }: Props) => {
   const navigation = useNavigate()
-
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const validateName = (value) => {
+    let error
+    if (!value) {
+      error = "Name is required"
+    } else if (value.toLowerCase() !== "naruto") {
+      error = "Jeez! You're not a fan 😱"
+    }
+    return error
+  }
   const handleCardClick = () => {
     navigation(name)
   }
+  const handleDelete = (event: React.MouseEvent) => {
+    event.stopPropagation()
+    onOpen()
+  }
   return (
-    <Card
-      borderRadius={10}
-      overflow="hidden"
-      cursor="pointer"
-      onClick={handleCardClick}>
+    <Card borderRadius={10} cursor="pointer" onClick={handleCardClick}>
       <CardBody>
-        <Heading fontSize="2xl">{name}</Heading>
+        <Heading fontSize="16px">{name}</Heading>
+        <Text fontSize="md">{baseUrl}</Text>
         <Text>{description}</Text>
+        <HStack marginTop="20px" justifyContent="space-between">
+          <Button
+            width="50%"
+            onClick={(event) => handleDelete(event)}
+            colorScheme="red"
+            variant="ghost">
+            delete
+          </Button>
+          <Button
+            width="50%"
+            onClick={(event) => handleDelete(event)}
+            colorScheme="teal"
+            variant="ghost">
+            edit
+          </Button>
+        </HStack>
       </CardBody>
+      <Modal onClose={onClose} size="xl" isOpen={isOpen}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Modal Title</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Formik
+              initialValues={{ name: "Sasuke" }}
+              onSubmit={(values, actions) => {
+                setTimeout(() => {
+                  alert(JSON.stringify(values, null, 2))
+                  actions.setSubmitting(false)
+                }, 1000)
+              }}>
+              {(props) => (
+                <Form>
+                  <Field name="name" validate={validateName}>
+                    {({ field, form }) => (
+                      <FormControl
+                        isInvalid={form.errors.name && form.touched.name}>
+                        <FormLabel>First name</FormLabel>
+                        <Input {...field} placeholder="name" />
+                        <FormErrorMessage>{form.errors.name}</FormErrorMessage>
+                      </FormControl>
+                    )}
+                  </Field>
+                  <HStack>
+                    <Spacer />
+                    <Button
+                      mt={4}
+                      mb={4}
+                      colorScheme="teal"
+                      isLoading={props.isSubmitting}
+                      type="submit">
+                      Submit
+                    </Button>
+                  </HStack>
+                </Form>
+              )}
+            </Formik>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
     </Card>
   )
 }
--- tabs/projectList.tsx
+++ tabs/projectList.tsx
@@ -8,28 +8,43 @@ export default function ProjectList() {
   console.log(location.pathname)
   const projectList = [
     {
-      name: "react-hooks",
-      desc: "react hooks 学习"
+      name: "react-hooks1react-hooks1",
+      desc: "react hooks 学习1",
+      baseUrl: "https://www.baidu.com"
     },
     {
       name: "react-hooks2",
-      desc: "react hooks 学习"
+      desc: "react hooks 学习2",
+      baseUrl: "https://www.baidu.com"
     },
     {
-      name: "react-hooks1",
-      desc: "react hooks 学习"
+      name: "react-hook11s3",
+      desc: "react hooks 学习3",
+      baseUrl: "https://www.baidu.com"
+    },
+    {
+      name: "react-hoo113ks3",
+      desc: "react hooks 学习3",
+      baseUrl: "https://www.baidu.com"
+    },
+    {
+      name: "react-hooks22",
+      desc: "react hooks 学习232",
+      baseUrl: "https://www.baidu.32com"
     }
   ]
   return (
     <>
       <SimpleGrid
-        templateColumns="repeat(auto-fill, minmax(200px, 1fr))"
-        spacing={10}>
+        templateColumns="repeat(auto-fill, minmax(250px, 1fr))"
+        spacing={10}
+        padding="20px">
         {projectList.map((project) => (
           <ProjectCard
             key={project.name}
             name={project.name}
             description={project.desc}
+            baseUrl={project.baseUrl}
           />
         ))}
       </SimpleGrid>
