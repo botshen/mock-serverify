b09664f3bc034edef7cb869e20d8c5779b24ed67
Xin shen <363088847@qq.com>
2024-01-06 22:14:35 +0800

    WIP: add sidebar

--- tabs/main.tsx
+++ tabs/main.tsx
@@ -1,31 +1,285 @@
-import { Grid, GridItem } from "@chakra-ui/react"
-import { Outlet } from "react-router-dom"
+"use client"
 
-import "../assets/style.scss"
+import { debug } from "console"
+import { MoonIcon, SunIcon } from "@chakra-ui/icons"
+import {
+  Avatar,
+  Box,
+  Button,
+  CloseButton,
+  Drawer,
+  DrawerContent,
+  Flex,
+  HStack,
+  Icon,
+  IconButton,
+  Menu,
+  MenuButton,
+  MenuDivider,
+  MenuItem,
+  MenuList,
+  Text,
+  useColorMode,
+  useColorModeValue,
+  useDisclosure,
+  VStack,
+  type BoxProps,
+  type FlexProps
+} from "@chakra-ui/react"
+import type { IconType } from "react-icons"
+import {
+  FiBell,
+  FiChevronDown,
+  FiCompass,
+  FiHome,
+  FiMenu,
+  FiSettings,
+  FiStar,
+  FiTrendingUp
+} from "react-icons/fi"
+import { Outlet, useLocation, useNavigate } from "react-router-dom"
 
-import NavBar from "./components/NavBar"
-import SideBar from "./components/SideBar"
+interface LinkItemProps {
+  name: string
+  icon: IconType
+}
 
-export default function Main() {
+interface NavItemProps extends FlexProps {
+  icon: IconType
+  children: React.ReactNode
+}
+
+interface MobileProps extends FlexProps {
+  onOpen: () => void
+}
+
+interface SidebarProps extends BoxProps {
+  onClose: () => void
+}
+const menu = [
+  {
+    name: "Projects",
+    link: "/projectList",
+    icon: FiHome
+  },
+  {
+    name: "Logs",
+    link: "/logs",
+    icon: FiTrendingUp
+  },
+  {
+    name: "Settings",
+    link: "/settings",
+    icon: FiSettings
+  },
+  {
+    name: "About",
+    link: "/about",
+    icon: FiCompass
+  }
+]
+
+const LinkItems: Array<LinkItemProps> = [
+  { name: "Home", icon: FiHome },
+  { name: "Trending", icon: FiTrendingUp },
+  { name: "Explore", icon: FiCompass },
+  { name: "Favourites", icon: FiStar },
+  { name: "Settings", icon: FiSettings }
+]
+
+const SidebarContent = ({ onClose, ...rest }: SidebarProps) => {
+  const navigate = useNavigate()
+  const location = useLocation()
+  const { colorMode } = useColorMode()
+
+  const handleMenuChange = (link: string) => {
+    if (location.pathname !== link) {
+      navigate(link)
+    }
+  }
+  const color = () => {
+    if (colorMode === "light") {
+      return "#51c3e7"
+    } else {
+      return "#9ae4d9"
+    }
+  }
   return (
-    <Grid
-      templateAreas={{
-        base: `"nav" "main"`,
-        lg: `"nav nav" "aside main"`
-      }}
-      templateColumns={{
-        base: "1fr",
-        lg: "250px 1fr"
-      }}>
-      <GridItem area="nav">
-        <NavBar />
-      </GridItem>
-      <GridItem area="aside">
-        <SideBar />
-      </GridItem>
-      <GridItem area="main">
-        <Outlet />
-      </GridItem>
-    </Grid>
+    <Box
+      transition="3s ease"
+      bg={useColorModeValue("white", "gray.900")}
+      borderRight="1px"
+      borderRightColor={useColorModeValue("gray.200", "gray.700")}
+      w={{ base: "full", md: 60 }}
+      pos="fixed"
+      h="full"
+      {...rest}>
+      <Flex h="20" alignItems="center" mx="8" justifyContent="space-between">
+        <Text fontSize="2xl" fontFamily="monospace" fontWeight="bold">
+          Logo
+        </Text>
+        <CloseButton display={{ base: "flex", md: "none" }} onClick={onClose} />
+      </Flex>
+      {menu.map((link) => (
+        <NavItem
+          key={link.name}
+          icon={link.icon}
+          bg={location.pathname === link.link ? color() : ""}
+          onClick={() => handleMenuChange(link.link)}>
+          <Text fontSize="xl">{link.name}</Text>
+        </NavItem>
+      ))}
+    </Box>
   )
 }
+
+const NavItem = ({ icon, children, ...rest }: NavItemProps) => {
+  return (
+    <Box
+      as="a"
+      style={{ textDecoration: "none" }}
+      _focus={{ boxShadow: "none" }}>
+      <Flex
+        align="center"
+        p="4"
+        mx="4"
+        borderRadius="lg"
+        role="group"
+        cursor="pointer"
+        _hover={{
+          bg: "cyan.400",
+          color: "white"
+        }}
+        {...rest}>
+        {icon && (
+          <Icon
+            mr="4"
+            fontSize="16"
+            _groupHover={{
+              color: "white"
+            }}
+            as={icon}
+          />
+        )}
+        {children}
+      </Flex>
+    </Box>
+  )
+}
+
+const MobileNav = ({ onOpen, ...rest }: MobileProps) => {
+  const { colorMode, toggleColorMode } = useColorMode()
+
+  return (
+    <Flex
+      ml={{ base: 0, md: 60 }}
+      px={{ base: 4, md: 4 }}
+      height="20"
+      alignItems="center"
+      bg={useColorModeValue("white", "gray.900")}
+      borderBottomWidth="1px"
+      borderBottomColor={useColorModeValue("gray.200", "gray.700")}
+      justifyContent={{ base: "space-between", md: "flex-end" }}
+      {...rest}>
+      <IconButton
+        display={{ base: "flex", md: "none" }}
+        onClick={onOpen}
+        variant="outline"
+        aria-label="open menu"
+        icon={<FiMenu />}
+      />
+
+      <Text
+        display={{ base: "flex", md: "none" }}
+        fontSize="2xl"
+        fontFamily="monospace"
+        fontWeight="bold">
+        Logo
+      </Text>
+
+      <HStack spacing={{ base: "0", md: "6" }}>
+        <IconButton
+          size="lg"
+          variant="ghost"
+          aria-label="open menu"
+          icon={colorMode === "light" ? <MoonIcon /> : <SunIcon />}
+          onClick={toggleColorMode}
+        />
+        {/* <Button onClick={toggleColorMode}>
+          {colorMode === "light" ? <MoonIcon /> : <SunIcon />}
+        </Button> */}
+        <Flex alignItems={"center"}>
+          <Menu>
+            <MenuButton
+              py={2}
+              transition="all 0.3s"
+              _focus={{ boxShadow: "none" }}>
+              <HStack>
+                <Avatar
+                  size={"sm"}
+                  src={
+                    "https://images.unsplash.com/photo-1619946794135-5bc917a27793?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9"
+                  }
+                />
+                <VStack
+                  display={{ base: "none", md: "flex" }}
+                  alignItems="flex-start"
+                  spacing="1px"
+                  ml="2">
+                  <Text fontSize="sm">Justina Clark</Text>
+                  <Text fontSize="xs" color="gray.600">
+                    Admin
+                  </Text>
+                </VStack>
+                <Box display={{ base: "none", md: "flex" }}>
+                  <FiChevronDown />
+                </Box>
+              </HStack>
+            </MenuButton>
+            <MenuList
+              bg={useColorModeValue("white", "gray.900")}
+              borderColor={useColorModeValue("gray.200", "gray.700")}>
+              <MenuItem>Profile</MenuItem>
+              <MenuItem>Settings</MenuItem>
+              <MenuItem>Billing</MenuItem>
+              <MenuDivider />
+              <MenuItem>Sign out</MenuItem>
+            </MenuList>
+          </Menu>
+        </Flex>
+      </HStack>
+    </Flex>
+  )
+}
+
+const Main = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure()
+
+  return (
+    <Box minH="100vh" bg={useColorModeValue("gray.100", "gray.900")}>
+      <SidebarContent
+        onClose={() => onClose}
+        display={{ base: "none", md: "block" }}
+      />
+      <Drawer
+        isOpen={isOpen}
+        placement="left"
+        onClose={onClose}
+        returnFocusOnClose={false}
+        onOverlayClick={onClose}
+        size="full">
+        <DrawerContent>
+          <SidebarContent onClose={onClose} />
+        </DrawerContent>
+      </Drawer>
+      <MobileNav onOpen={onOpen} />
+
+      <Box ml={{ base: 0, md: 60 }} p="4">
+        {/* Content */}
+        <Outlet />
+      </Box>
+    </Box>
+  )
+}
+
+export default Main
---
+++ tabs/main——1.tsx
@@ -0,0 +1,31 @@
+import { Grid, GridItem } from "@chakra-ui/react"
+import { Outlet } from "react-router-dom"
+
+import "../assets/style.scss"
+
+import NavBar from "./components/NavBar"
+import SideBar from "./components/SideBar"
+
+export default function Main() {
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`
+      }}
+      templateColumns={{
+        base: "1fr",
+        lg: "250px 1fr"
+      }}>
+      <GridItem area="nav">
+        <NavBar />
+      </GridItem>
+      <GridItem area="aside">
+        <SideBar />
+      </GridItem>
+      <GridItem area="main">
+        <Outlet />
+      </GridItem>
+    </Grid>
+  )
+}
